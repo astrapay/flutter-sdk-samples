04179efa7b447c18915327002de2cf396d9f85a3
dyas.i.masruri <dyas.mail@gmail.com>
2022-04-08 11:40:24 +0700

    add webview top up

--- android/app/src/main/kotlin/com/example/flutter_astrapay_sdk_sample/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_astrapay_sdk_sample/MainActivity.kt
@@ -21,6 +21,8 @@ import com.astrapay.qrissdk.helper.AstraPayQris
 import com.astrapay.qrissdk.helper.AstraPayQrisListener
 import com.astrapay.qrissdk.helper.EventType
 import com.astrapay.qrissdk.helper.data.AstraPayQrisAuth
+import com.astrapay.webviewtopupsdk.helper.AstraPayWebViewTopUp
+import com.astrapay.webviewtopupsdk.helper.WebViewTopUpAuth
 
 class MainActivity: FlutterActivity(), AstraPayQrisListener {
     val CHANNEL = "channel_qris"
@@ -33,7 +35,7 @@ class MainActivity: FlutterActivity(), AstraPayQrisListener {
                 AstraPayQris.setup("XTOKEN", "SIT")
                 val astraPayAuth = AstraPayQrisAuth(
                     context = this,
-                    authorizationToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzUxMiJ9.eyJzdWIiOiIwODc3ODU0NTEyODkiLCJyb2xlcyI6WyJMT0dJTiJdLCJpc3MiOiJBc3RyYVBheS1EZXYiLCJ0eXBlIjoiQUNDRVNTIiwidXNlcklkIjo1ODEsImRldmljZUlkIjoiMzFhNzMzMDYtYWU5MS00ZjZiLTliOTgtYjc3YzZiYjM1YWI5IiwidHJhbnNhY3Rpb25JZCI6IiIsInRyYW5zYWN0aW9uVHlwZSI6IiIsIm5iZiI6MTY0MDgzOTQzNiwiZXhwIjoxNjQwODQzMDM2LCJpYXQiOjE2NDA4Mzk0MzYsImp0aSI6IjE5ZWVlMjk4LWVkZWQtNDc0Zi1hMTU5LTdlMmJhYjhlNWMwMiIsImVtYWlsIjpbImphc29ubmF0aGFuYWVsMThAZ21haWwuY29tIl19.bx6B3DTOJvsg4O2TQ18wL_RxVBjzWCR38GEQX5YfywXj-sAKQyRHTxpKjh8GnuZ_uyxmklFjeyxdyN-qNOifzHcyQye82Gzu0-kn6uNL52WbJoA3_1XA6KUwk6lexbl80BJqXeQ8R9-slOm9YXotCl6ekqIJLfjaZF9R7gpwTAHphHrOhY5hXOtAVMDr3GOUWRv-1XdkQK4yXPqkN1_Ohjmw3JSz5CFilmgf5ACdqAteZXEig9gvWBBV12eWgjfnJhlL2Gr4_PaJ9ip8qbM1LNLz2cMG2CaEwhIpNiYwyzqsVv4gfQIn9fKuB7vLGSduhFAtVsK_8LLFKYsngOi2eg",
+                    authorizationToken = "AUTH_TOKEN",
                     listener = this
                 )
 
@@ -41,6 +43,15 @@ class MainActivity: FlutterActivity(), AstraPayQrisListener {
                     astraPayQrisAuth = astraPayAuth
                 )
 
+            } else if (call.method == "startWebViewTopUp") {
+                val webViewAuth = WebViewTopUpAuth(
+                    context = this,
+                    authorizationToken = "AUTH_TOKEN"
+                )
+                AstraPayWebViewTopUp.execute(
+                    webViewTopUpAuth = webViewAuth,
+                    buildType = AstraPayWebViewTopUp.Build.UAT
+                )
             } else{
                 result.notImplemented()
             }
